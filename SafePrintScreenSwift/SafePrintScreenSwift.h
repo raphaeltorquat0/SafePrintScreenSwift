723204632949dcbed23c3ab16d6ebbb6c0ce48a2
Raphael Torquato <martinitorquato@gmail.com>
2023-05-09 17:48:24 -0300

    Initial Commit

---
+++ SafePrintScreenSwift/SafePrintScreenSwift.h
@@ -0,0 +1,18 @@
+//
+//  SafePrintScreenSwift.h
+//  SafePrintScreenSwift
+//
+//  Created by Raphael Torquato on 09/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SafePrintScreenSwift.
+FOUNDATION_EXPORT double SafePrintScreenSwiftVersionNumber;
+
+//! Project version string for SafePrintScreenSwift.
+FOUNDATION_EXPORT const unsigned char SafePrintScreenSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafePrintScreenSwift/PublicHeader.h>
+
+
